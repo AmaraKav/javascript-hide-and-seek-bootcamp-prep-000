c79b058dcd24aaf97d1667907de910ad77ace2ba
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-11-27 20:56:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n){
+  var select = document.querySelector('.ranked-list');
+  for(var i = 0; i < select.length; i++){
+    var children = select[i].innerHTML;
+    for(var x = 0; x < children.length; x++){
+      var number = parseInt(children[x].innerHTML);
+      var increase = number + n;
+    }
+  }
+}
