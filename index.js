c0647005a3da677c339a57c034063f24b092b65d
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-11-27 20:58:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function increaseRankBy(n){
   for(var i = 0; i < select.length; i++){
     var children = select[i].innerHTML;
     for(var x = 0; x < children.length; x++){
-      var children[x].innerHTML = parseInt(children[x].innerHTML) + n;
+      children[x].innerHTML = parseInt(children[x].innerHTML) + n;
     }
   }
 }
